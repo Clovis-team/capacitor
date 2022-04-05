38d919bdc89a1013a0c1a60ec8d1b89d71469d24
avallete <andrew@clovis.pro>
2022-04-05 17:35:51 +0200

    fix(android): webview redirect for auth0

--- android/capacitor/src/main/java/com/getcapacitor/WebViewLocalServer.java
+++ android/capacitor/src/main/java/com/getcapacitor/WebViewLocalServer.java
@@ -353,10 +353,34 @@ public class WebViewLocalServer {
                         String base64 = Base64.encodeToString(userInfoBytes, Base64.NO_WRAP);
                         conn.setRequestProperty("Authorization", "Basic " + base64);
                     }
+                    /**
+                     * This should be the final step in the setup process of the HttpURLConnection object.
+                     *
+                     * Initiate the connection with follow redirects disable. It allows us
+                     * identifying any redirection response and avoid intercepting such request.
+                     * This is because returning a 200 response when a redirection response (ie. 301 or 302)
+                     * is expected could result in issues with resolving relative URLs for static assets
+                     * in the HTML.
+                     */
+                    conn.setInstanceFollowRedirects(false);
+                    conn.connect();
+
                     String cookie = conn.getHeaderField("Set-Cookie");
                     if (cookie != null) {
                         CookieManager.getInstance().setCookie(url, cookie);
                     }
+
+                    int status = conn.getResponseCode();
+                    if (
+                        status == HttpURLConnection.HTTP_MOVED_TEMP ||
+                        status == HttpURLConnection.HTTP_MOVED_PERM ||
+                        status == HttpURLConnection.HTTP_SEE_OTHER ||
+                        status == 307 // Temporary Redirect
+                    ) {
+                        // Return null so that this request will not be intercepted.
+                        return null;
+                    }
+
                     InputStream responseStream = conn.getInputStream();
                     responseStream = jsInjector.getInjectedStream(responseStream);
                     return new WebResourceResponse(
